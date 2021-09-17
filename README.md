a57a79c78667f278b55d9563684e9bc6653b99b5
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2021-09-17 20:57:09 +0100

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,9 +0,0 @@
-- 👋 Hi, I’m @amine-ziad-ounnoughene
-- 👀 I’m interested in AI,technology and science in general
-- 🌱 I’m currently learning DATA SCIENCE
-- 💞️ I’m looking to collaborate on PROJECTS THAT WILL CHANGE THE WORLD FOR BETTER
-
-<!---
-amine-ziad-ounnoughene/amine-ziad-ounnoughene is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
---
+++ README.py
@@ -0,0 +1,29 @@
+# 👋 Hi, I’m @amine-ziad-ounnoughene
+class Amine_Ziad_Ounnoughene :
+      def __init__ :
+          Self.age = 14
+          Self.country = "Algeria 🇩🇿🇩🇿"
+          Self.educational_level = 'high school🏢'
+          self.founder = "devos"
+      def get_programming_languages(self):
+          return [[dart🎯",50%],["python🐍",80%],["julia",30%],["SQL",50%]]
+      def get_databases(self):
+          return "mySQL"
+      def get_machine_learning_algorithms(self):
+          return ["linear regression","KNN","Kmeans","PCA","support vector machines","logistic regression","naive bayes"]
+      def get_deep_learnig_models(self):  
+          return ["autorncoders","VAE","FNN’S","RNN’S","CNN’S","transformers","LSTM’S,GRU’S"]
+      def get_python_packages(self):
+          return ["numpy","pandas","sklearn","opencv","streamlit", "tensorflow", "matplotlib","plotly.express"]
+      def get_hobbies_interests_and_skills(self):
+          return ["math♾","swiming🏊‍","cyclisme🚲","traveling✈","digital marketing"]
+      def get_future_plans(self)
+        hobbies_interests_and_skills = self.get_hobbies_interests_and_skills()
+        plans = ["learn_quantum_machine_learning","share_my_humbble_knowledge"]
+        for plan in plans:
+          hobbies_interests_and_skills.append(plan)
+        programming = self.get_programming_languages()
+        for i in len(programming):
+          programming[i][1] += 20
+        return hobbies_interests_and_skills,programming
+          
